1bf4092c55251152ccfb6a1ce59880d29e2e058e
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-04-27 02:26:57 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,10 +1,18 @@
 #include "alerter.h"
-
+#define THRESHOLD 200
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+      if(celcius>THRESHOLD)
+     {
+        return 500;
+     }
+    else
+    {        
+        return 200;
+    }
     return 200;
 }
 
