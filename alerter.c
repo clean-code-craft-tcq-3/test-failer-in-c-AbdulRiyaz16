2670b65a5f9ae05a651ec79123ef5e18039c5c92
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-08 21:47:43 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,19 +1,17 @@
 #include "alerter.h"
 #define THRESHOLD 200
 int networkAlertStub(float celcius) {
+    int temperature=0;
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-      if(celcius>THRESHOLD)
-     {
-        return 500;
-     }
+    if(celcius>THRESHOLD)
+    {
+        temperature=500;
+    }
     else
     {        
-        return 200;
+        temperature=200;
     }
-    return 200;
+    return temperature;
 }
 
 float farenheitToCelcius(float farenheit)
@@ -24,12 +22,9 @@ float farenheitToCelcius(float farenheit)
 void alertInCelcius(float farenheit) {
     float celcius = farenheitToCelcius(farenheit);
     int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+    if (returnCode != 200) 
+    {
+        alertFailureCount += 1;
     }
 }
 
